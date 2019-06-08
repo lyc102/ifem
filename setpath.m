108cb1a210479ef71e4c67f10e902e0ec4cd0735
ifem <lyc102@gmail.com>
2019-06-08 14:37:24 -0700

    add setpath and .gitignore file

---
+++ setpath.m
@@ -0,0 +1,8 @@
+%% Set path to iFEM  
+%
+% Add all subdirectories under pathstr to search path.
+%
+% Copyright (C) Long Chen. See COPYRIGHT.txt for details. 
+
+addpath(genpath(pwd),'-begin');
+savepath;
