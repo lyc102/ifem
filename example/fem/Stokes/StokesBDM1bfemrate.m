78774a76a827bc25c6eee97fa6955aa093962cf8
ifem <lyc102@gmail.com>
2019-06-08 15:34:37 -0700

    correct the input in StokesRT and BDM femrate files

--- example/fem/Stokes/StokesBDM1bfemrate.m
+++ example/fem/Stokes/StokesBDM1bfemrate.m
@@ -37,6 +37,7 @@ close all;
 [node,elem] = squaremesh([0,1,0,1],0.5);
 pde = Stokesdata1; % zero Dirichlet boundary condition
 bdFlag = setboundary(node,elem,'Dirichlet');
+mesh = struct('node',node,'elem',elem,'bdFlag',bdFlag);
 % option
 option.L0 = 1;
 option.elemType = 'BDM1B-P0';
@@ -44,7 +45,7 @@ option.maxIt = 4;
 option.solver = 'mg';
 option.refType = 'bisect';
 % fem
-femStokesHdiv(node,elem,bdFlag,pde,option);
+femStokesHdiv(mesh,pde,option);
 %% 
 % For bisection grids, only half order for vorticity and pressure. It is
 % interesting to note that the energy norm $||u_I - u_h||_1$ doesn't converge
@@ -59,6 +60,7 @@ close all;
 [node,elem] = squaremesh([0,1,0,1],0.5);
 pde = Stokesdata1;  % non-zero Dirichlet boundary condition
 bdFlag = setboundary(node,elem,'Dirichlet');
+mesh = struct('node',node,'elem',elem,'bdFlag',bdFlag);
 % option
 option.L0 = 1;
 option.elemType = 'BDM1B-P0';
@@ -67,7 +69,7 @@ option.solver = 'mg';
 option.printlevel = 1;
 option.refType = 'red';
 % fem
-femStokesHdiv(node,elem,bdFlag,pde,option);
+femStokesHdiv(mesh,pde,option);
 %%
 % The same pde data is used but the refinement rule is changed to red
 % refinement. For red refinement grids and zero Dirichlet boundary
--- example/fem/Stokes/StokesRT0femrate.m
+++ example/fem/Stokes/StokesRT0femrate.m
@@ -42,6 +42,7 @@ close all;
 [node,elem] = squaremesh([0,1,0,1],0.5);
 pde = Stokesdata1; % zero Dirichlet boundary condition
 bdFlag = setboundary(node,elem,'Dirichlet');
+mesh = struct('node',node,'elem',elem,'bdFlag',bdFlag);
 % option
 option.L0 = 1;
 option.elemType = 'RT0-P0';
@@ -49,7 +50,7 @@ option.maxIt = 4;
 option.solver = 'mg';
 option.refType = 'bisect';
 % fem
-femStokesHdiv(node,elem,bdFlag,pde,option);
+femStokesHdiv(mesh,pde,option);
 %% 
 % For bisection grids, only half order for vorticity and pressure. It is
 % interesting to note that the energy norm $||u_I - u_h||_1$ doesn't converge
@@ -64,6 +65,7 @@ close all;
 [node,elem] = squaremesh([0,1,0,1],0.5);
 pde = Stokesdata1;  % non-zero Dirichlet boundary condition
 bdFlag = setboundary(node,elem,'Dirichlet');
+mesh = struct('node',node,'elem',elem,'bdFlag',bdFlag);
 % option
 option.L0 = 1;
 option.elemType = 'RT0-P0';
@@ -72,7 +74,7 @@ option.solver = 'mg';
 option.printlevel = 1;
 option.refType = 'red';
 % fem
-femStokesHdiv(node,elem,bdFlag,pde,option);
+femStokesHdiv(mesh,pde,option);
 %%
 % The same pde data is used but the refinement rule is changed to red
 % refinement. For red refinement grids and zero Dirichlet boundary
