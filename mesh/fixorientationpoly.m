e02965fb7629160d3b95d3511226c5c0c82c59da
Shuhao Cao <scao.math@gmail.com>
2020-05-05 23:59:28 -0700

    Create fixorientationpoly.m

---
+++ mesh/fixorientationpoly.m
@@ -0,0 +1,33 @@
+function elem = fixorientationpoly(node,elem)
+%% FIXORIENTATIONPOLY set all polygon clockwise
+% 
+%   elem = fixorientationpoly(node,elem) reorder the elem to orient
+%   clockwise similar to the quad element set up. 
+%   elem{:} has to be a cell array.
+%
+%
+% See also fixorder, fixorder3
+%
+% Copyright (C) Long Chen. See COPYRIGHT.txt for details.
+
+elem = cellfun(@(x) reorder(x), elem, 'UniformOutput',false);
+
+%%
+    function idx = reorder(idx)
+        [xc, yc] = centroid(node(idx,:));
+        vecx = node(idx,:) - [xc, yc];
+        [~, idxOrdered] = sort(atan2(vecx(:,1), vecx(:,2)));
+        idx = idx(idxOrdered);
+    end
+
+    function [xc, yc] = centroid(p)
+        x1 = p(:,1);
+        y1 = p(:,2);
+        x2 = circshift(x1,-1);
+        y2 = circshift(y1,-1);
+        bdIntegral = x1.*y2 - y1.*x2;
+        area = sum(bdIntegral)/2;
+        xc = sum((x1+x2).*bdIntegral) /6/area;
+        yc = sum((y1+y2).*bdIntegral) /6/area;
+    end
+end
