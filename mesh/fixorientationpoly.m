7e59501284c1efe85c2fcc197b2c3c86436d3cba
Shuhao Cao <scao.math@gmail.com>
2020-05-11 11:18:18 -0700

    Update fixorientationpoly.m

--- mesh/fixorientationpoly.m
+++ mesh/fixorientationpoly.m
@@ -3,36 +3,45 @@ function elem = fixorientationpoly(node,elem,varargin)
 % 
 %   elem = fixorientationpoly(node,elem) reorder the elem to orient
 %   clockwise similar to the quad element set up. 
-%   elem{:} has to be a cell array.
+%   elem{:} has to be a cell array. Default is counter-clockwise order.
 %
 %
 % See also fixorder, fixorder3
 %
 % Copyright (C) Long Chen. See COPYRIGHT.txt for details.
 
-elem = cellfun(@(x) reorder(x), elem,'UniformOutput',false);
-if nargin > 2
-    if strcmp(varargin{1}, 'CounterClockwise')
-        elem = cellfun(@(x) fliplr(x), elem, 'UniformOutput',false);
+elemVertexNumber = cellfun('length',elem);
+maxNv = max(elemVertexNumber);
+minNv = min(elemVertexNumber);
+for Nv = minNv:maxNv
+    isNv = (elemVertexNumber == Nv);
+    if any(isNv)
+        elemNv = cell2mat(elem(isNv,:));
+        % number of elements sharing the same # of vertices
+        NelemNv = sum(isNv);
+        
+        x1 = reshape(node(elemNv,1),NelemNv,Nv);
+        y1 = reshape(node(elemNv,2),NelemNv,Nv);
+        xc = mean(x1,2)-eps; yc = mean(y1,2); % simple mean with a shift
+        nodeNv = node(elemNv',:);
+        nodeNv2elem = reshape(nodeNv', [2, Nv, NelemNv]);
+        nodeNv2elem = permute(nodeNv2elem, [3 1 2]);
+        vert2Centroid = nodeNv2elem - repmat([xc, yc], [1 1 Nv]);
+        vert2CentroidAngle = zeros(NelemNv,Nv);
+        for j = 1:Nv
+            vert2CentroidAngle(:,j) = atan2(vert2Centroid(:,2,j), vert2Centroid(:,1,j));
+        end
+        [~, ixSorted] = sort(vert2CentroidAngle,2);
+        ixSorted = repmat(0:Nv:Nv*(NelemNv-1), [Nv 1]) + ixSorted';
+        elemNv = elemNv';
+        elemNvSorted = reshape(elemNv(ixSorted(:)), [Nv,NelemNv])';
+        elem(isNv) = num2cell(elemNvSorted,2);
     end
 end
 
-%%
-    function idx = reorder(idx)
-        [xc, yc] = centroid(node(idx,:));
-        vecx = node(idx,:) - [xc, yc];
-        [~, idxOrdered] = sort(atan2(vecx(:,1), vecx(:,2)));
-        idx = idx(idxOrdered);
+if nargin > 2
+    if strcmp(varargin{1}, 'Clockwise')
+        elem = cellfun(@(x) fliplr(x), elem, 'UniformOutput',false);
     end
-
-    function [xc, yc] = centroid(p)
-        x1 = p(:,1);
-        y1 = p(:,2);
-        x2 = circshift(x1,-1);
-        y2 = circshift(y1,-1);
-        bdIntegral = x1.*y2 - y1.*x2;
-        area = sum(bdIntegral)/2;
-        xc = sum((x1+x2).*bdIntegral) /6/area;
-        yc = sum((y1+y2).*bdIntegral) /6/area;
-    end
+end
 end
