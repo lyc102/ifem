66bad953e6c89b27311deed330d4cc1deecd7683
ifem <lyc102@gmail.com>
2020-08-28 23:33:54 -0700

    correct cubemesh

--- mesh/cubemesh.m
+++ mesh/cubemesh.m
@@ -1,11 +1,12 @@
-function [node,elem,HB] = cubemesh(box,varargin)
+function [node,elem,HB] = cubemesh(box,h)
 %% CUBEMESH a uniform mesh of a cube
 %
 % [node,elem,HB] = cubemesh([x0,x1,y0,y1,z0,z1],h) generates a uniform mesh
 % of the cube [x0,x1]*[y0,y1]*[z0,z1] with certain mesh size h. 
 %
-% Default mode: the number of cuts along x, y, z direction are equal.
-% Additional option: different mesh size along x, y, z direction.
+% [node,elem,HB] = cubemesh([x0,x1,y0,y1,z0,z1],[hx,hy,hz]) generates a
+% uniform mesh of the cube [x0,x1]*[y0,y1]*[z0,z1] with mesh size
+% hx in x-direction, hy in y-direction, and hz in z-direction.
 %
 % Example 1
 %
@@ -14,13 +15,12 @@ function [node,elem,HB] = cubemesh(box,varargin)
 %
 % Example 2
 %
-%  [node,elem,HB] = cubemesh([-1,1,-1,1,-1,1],0.5);
+%  [node,elem,HB] = cubemesh([0,1,0,2,0,4],1);
 %  showmesh3(node,elem);
 % 
 % Example 3
 %
-%  option.hx = 1/2; option.hy = 1/3; option.hz = 1;
-%  [node,elem,HB] = cubemesh([0,1,0,1,0,1],option);
+%  [node,elem,HB] = cubemesh([0,1,0,1,0,1],[0.5, 0.25, 0.125]);
 %  showmesh3(node,elem);
 %
 % The ordering of vertices is important for the uniform refinement. See
@@ -42,41 +42,38 @@ node = [x0,y0,z0; x1,y0,z0; x1,y1,z0; x0,y1,z0; ...
 elem = [1 2 3 7; 1 4 3 7; 1 5 6 7; 1 5 8 7; 1 2 6 7; 1 4 8 7];
 % The order of vertices is important for the uniform refinement.
 
-if nargin ==1
-    h = min(abs([x1- x0, y1-y0, z1-z0]/2));
-    
-    n = max(ceil(log2(abs([x1- x0, y1-y0, z1-z0]/h))));
-    for k = 1:n
-        [node,elem] = uniformrefine3(node,elem);
-    end
-    
-elseif (nargin >= 2) && isnumeric(varargin{1})
-    h = varargin{1};
-    n = max(ceil(log2(abs([x1- x0, y1-y0, z1-z0]/h))));
-    for k = 1:n
-        [node,elem] = uniformrefine3(node,elem);
-    end
-      
-elseif  (nargin >= 2) && ~isnumeric(varargin{1})
-    option = varargin{1};
-    hx = option.hx; hy = option.hy; hz = option.hz;
-    
-    hx = (x1- x0)/floor(abs((x1- x0)/hx));
-    hy = (x1- x0)/floor(abs((x1- x0)/hy));
-    hz = (x1- x0)/floor(abs((x1- x0)/hz));
-  
-    [x,y,z] = ndgrid(x0:hx:x1,y0:hy:y1,z0:hz:z1);
+if (nargin >= 2) 
+    if length(h) == 1       
+        [x,y,z] = meshgrid(x0:h:x1,y0:h:y1,z0:h:z1);
+    elseif length(h) == 3
+        hx = (x1- x0)/floor(abs((x1- x0)/h(1)));
+        hy = (x1- x0)/floor(abs((x1- x0)/h(2)));
+        hz = (x1- x0)/floor(abs((x1- x0)/h(3)));
+        [x,y,z] = meshgrid(x0:hx:x1,y0:hy:y1,z0:hz:z1);
+    end  
     node = [x(:),y(:),z(:)];
-
-    matlabversion = version();
-    if str2double(matlabversion(end-5:end-2)) <= 2013
-        T = DelaunayTri(x(:),y(:),z(:)); %#ok<*DDELTRI>
-        elem = T.Triangulation;
-    else
-        T = delaunayTriangulation(x(:),y(:),z(:));
-        elem = T.ConnectivityList;
+    [nx,ny,nz] = size(x);
+    elem = zeros(6*(nx-1)*(ny-1)*(nz-1),4);
+    indexMap = reshape(1:nx*ny*nz,nx,ny,nz);
+    localIndex = zeros(8,1);
+    idx = 1;
+    for i = 1:nx-1
+        for j = 1:ny-1
+            for k = 1:nz-1
+                localIndex(1) = indexMap(i,j,k);
+                localIndex(2) = indexMap(i,j+1,k);
+                localIndex(3) = indexMap(i+1,j+1,k);
+                localIndex(4) = indexMap(i+1,j,k);
+                localIndex(5) = indexMap(i,j,k+1);
+                localIndex(6) = indexMap(i,j+1,k+1);
+                localIndex(7) = indexMap(i+1,j+1,k+1);
+                localIndex(8) = indexMap(i+1,j,k+1);
+                elem(idx:idx+5,:) = localIndex([1 2 3 7; 1 4 3 7; 1 5 6 7;...
+                                                1 5 8 7; 1 2 6 7; 1 4 8 7]);
+                idx = idx + 6;
+            end
+        end
     end
-    elem = label3(node,elem);
 end
 
 % Set this as an initial grid
