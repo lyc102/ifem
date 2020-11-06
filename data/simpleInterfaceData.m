a82fae0c64375884cb12d97ea08f02612f2decaa
Shuhao Cao <scao.math@gmail.com>
2020-11-06 12:21:44 -0600

    Update simpleInterfaceData.m

--- data/simpleInterfaceData.m
+++ data/simpleInterfaceData.m
@@ -1,20 +1,25 @@
-function pde = simpleInterfaceData
+function pde = simpleInterfaceData(varargin)
 %%  SIMPLEINTERFACE data of the elliptic interface problem
 %
 % Copyright (C) Long Chen. See COPYRIGHT.txt for details.
 
-pde = struct('f',0,'exactu',@exactu,'g_D',@exactu,'Du',@Du,'d',@d, 'phi', @phi);
+pde = struct('f',@f,'exactu',@exactu,'g_D',@exactu,'Du',@Du,'d',@d, 'phi', @phi);
 
-
-r0 = pi/6.28;
+if isempty(varargin)
+    betaMinus = 1;
+    betaPlus = 5;
+else
+    betaMinus = varargin{1};
+    betaPlus = varargin{2};
+end
+r0 = pi/6;
 phi = @(p) p(:,1).^2 + p(:,2).^2 - r0.^2;
-betaMinus = 1;
-betaPlus = 10;
+
 alpha = 5;
 
     function K =  d(p)  % Diffusion constant
         idxPlus = (phi(p) >0);
-        K = ones(size(p,1),1);
+        K = betaMinus*ones(size(p,1),1);
         K(idxPlus) = betaPlus;
     end
 
@@ -23,17 +28,22 @@ alpha = 5;
         idxMinus = 1 - idxPlus;
         r2 = p(:,1).^2 + p(:,2).^2;
         u = r2.^(alpha/2)./betaMinus.*idxMinus + ...
-            (r2.^(alpha/2)./betaPlus + 1/betaMinus - 1/betaPlus).*idxPlus;
+            (r2.^(alpha/2)./betaPlus + r0^alpha*(1/betaMinus - 1/betaPlus)).*idxPlus;
     end
 
-    function Du = Du(p)
+    function uprime = Du(p)
         idxPlus = (phi(p) >0);
         idxMinus = 1 - idxPlus;
         r2 = p(:,1).^2 + p(:,2).^2;
-        Du(:,1) = 2*p(:,1).*r2.*(alpha/2-1)./betaMinus.*idxMinus + ...
-            2*p(:,1).*r2.*(alpha/2-1)./betaPlus.*idxPlus;
-        Du(:,2) = 2*p(:,2).*r2.*(alpha/2-1)./betaMinus.*idxMinus + ...
-            2*p(:,2).*r2.*(alpha/2-1)./betaPlus.*idxPlus;
+        uprime(:,1) = alpha*(p(:,1).*r2.*(alpha/2-1)./betaMinus.*idxMinus + ...
+            p(:,1).*r2.*(alpha/2-1)./betaPlus.*idxPlus);
+        uprime(:,2) = alpha*(p(:,2).*r2.*(alpha/2-1)./betaMinus.*idxMinus + ...
+            p(:,2).*r2.*(alpha/2-1)./betaPlus.*idxPlus);
+    end
+
+    function s = f(p)
+        r2 = p(:,1).^2 + p(:,2).^2;
+        s = -alpha^2*r2.^(alpha/2-1);
     end
 
 
