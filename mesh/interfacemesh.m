2e899f9f15d4e9bc1db00ec88b21492bd4057c54
Long Chen <lyc102@gmail.com>
2021-08-02 14:54:35 -0700

    add link to interfacemesh2demo in example folder

--- mesh/interfacemesh.m
+++ mesh/interfacemesh.m
@@ -15,11 +15,19 @@ function [node,elem,interface] = interfacemesh(box,phi,h)
 %   - interface.edge: edges approximating the interface;
 %   - interface.node: vertices on the interface;
 %
-%  Example
+%  Examples
+%   % circle
 %     box = [ -1, 1, -1, 1];
 %     h = 0.1;
 %     phi = @(p) sum(p.^2, 2) - 0.5.^2;
-%     [node,elem,interfaceData] = interfacemesh(box,phi,h);
+%     [node,elem,interfaceData] = interfacemeshdoc(box,phi,h);
+%
+%    % heart
+%     box = [ -1, 1, -1, 1];
+%    [node,elem,interface] = interfacemeshdoc(box,@phiheart,0.05);
+%    showmesh(node,elem);
+%    findedge(node,interface.edge,'all','noindex','draw');
+%
 %
 % Author: Huayi Wei <weihuayi@xtu.edu.cn>, and Long Chen.
 %
@@ -32,15 +40,16 @@ edge2elem = T.edge2elem;
 clear T;
 N = size(node,1);
 NT = size(elem,1);
-% compute the phi value at each vertex
-phiValue = phi(node);
-phiValue(abs(phiValue) < 0.1*h^2) = 0;  % treat nearby nodes as on the interface
-vSign = msign(phiValue);
-showmesh(node,elem);
 
 %% Step 1: Find points near interface 
+% compute the phi value at each vertex
+phiValue = phi(node);
+phiValue(abs(phiValue) < eps*h) = 0;  % treat nearby nodes as on the interface
+vSign = sign(phiValue);
+
 % Find the intersection points between edges and the interface
 isCutEdge = (vSign(edge(:,1)).* vSign(edge(:,2))<0);
+findedge(node,edge,isCutEdge,'noindex','draw');
 A = node(edge(isCutEdge,1),:);
 B = node(edge(isCutEdge,2),:);
 cutNode = findintersectbisect(phi,A,B);
@@ -50,63 +59,65 @@ vSign(N+1:N+Ncut) = 0;
 % find interface elem and nodes
 isInterfaceElem = false(NT,1);  
 isInterfaceElem(edge2elem(isCutEdge,[1,2])) = true;
-isInterfaceElem(sum(abs(vSign(elem)), 2) < 3) = true; % 1 or 2 vertices on interface
+isInterfaceElem(sum(abs(vSign(elem)), 2) < 3) = true; % 2 vertices on interface
 isInterfaceNode = false(N,1);
 isInterfaceNode(elem(isInterfaceElem,:)) = true;
 
-% add centers of special elements
-%  0 - -1    -1 - 0     0 -  0
-%  1 -  0     0 - 1     1 - -1
-isSpecialElem = (sum(vSign(elem),2) == 0) & (sum(abs(vSign(elem)),2) == 2);
-selem = elem(isSpecialElem,:);
-% eliminate the third case
-isSpecialElem = (vSign(selem(:,1)).*vSign(selem(:,3)) == -1) | ...
-                (vSign(selem(:,2)).*vSign(selem(:,4)) == -1);
-selem = selem(isSpecialElem,:);
-auxPoint = (node(selem(:,1),:) + node(selem(:, 3),:))/2.0;
+% add centers of special elements (two vertices on the interface)
+%  0 - -1    -1 - 0     0 -  0      1 - 0
+%  1 -  0     0 - 1     1 - -1      1 - 0
+isSpecialElem = (sum(abs(vSign(elem)),2) <= 2); % two or more vertices on interfaces
+% isSpecialElem = (sum(vSign(elem),2) == 0) & (sum(abs(vSign(elem)),2) == 2);
+specialElem = elem(isSpecialElem,:);
+auxPoint = (node(specialElem(:,1),:) + node(specialElem(:, 3),:))/2.0;
 Naux = size(auxPoint,1);
+vSign(N+Ncut+1:N+Ncut+Naux) = sign(phi(auxPoint));
+% find the first two cases
+isDiagInterface = (vSign(specialElem(:,1)).*vSign(specialElem(:,3)) == -1) | ...
+                  (vSign(specialElem(:,2)).*vSign(specialElem(:,4)) == -1);
+vSign(N+Ncut+find(isDiagInterface)) = 0;            
 
 % add cut points and aux points
 node = [node; cutNode; auxPoint];
-interfaceNode = [node(isInterfaceNode,:); cutNode; auxPoint];
+nearInterfaceNode = [node(isInterfaceNode,:); cutNode; auxPoint];
 interfaceNodeIdx = [find(isInterfaceNode); N+(1:Ncut)'; N+Ncut+(1:Naux)'];
-vSign(N+Ncut+1:N+Ncut+Naux) = 0;
 
 %% Step 2: generate a Delaunay triangulation of interface points
 % construct the Delaunay triangulation of interfaceNode
 % different versions of matlab using different delaunay triangulation
 matlabversion = version();
 if str2double(matlabversion(end-5:end-2)) <= 2013
-    DT = DelaunayTri(interfaceNode); %#ok<*DDELTRI>
+    DT = DelaunayTri(nearInterfaceNode); %#ok<*DDELTRI>
     tElem = DT.Triangulation;
 else
-    DT = delaunayTriangulation(interfaceNode);
+    DT = delaunayTriangulation(nearInterfaceNode);
     tElem = DT.ConnectivityList;
 end
-tElem = fixorder(interfaceNode,tElem); % correct the orientation
+tElem = fixorder(nearInterfaceNode,tElem); % correct the orientation
 
 %% Step 3: Post-processing
 % get rid of the unnecessary triangles
-NI = sum(isInterfaceNode);  % number of vertices of interface elements
-isUnnecessaryElem = (sum(tElem<=NI,2) == 3); % if three vertices are vertices
-tElem = tElem(~isUnnecessaryElem,:); % of interface elements, it is not needed
+NI = sum(isInterfaceNode);  % number of pts of the background mesh near interface
+haveNewPts = (sum(tElem > NI,2) > 0); % triangles containing new added vertices
+% vSignNearInterface = zeros(size(nearInterfaceNode,1),1);
+% vSignNearInterface(1:sum(isInterfaceNode)) = vSign(isInterfaceNode);
+% haveInterfacePts = (sum(abs(vSignNearInterface(tElem)),2) < 3);
+tElem = tElem(haveNewPts,:); 
+% tElem = tElem(haveInterfacePts,:); 
 tElem = interfaceNodeIdx(tElem);  % map interfaceNode index to node index
-showmesh(node,tElem,'Facecolor','y');
+
 % get the remainding quad elems
 sElem = elem(~isInterfaceElem,:);
 % merge into one triangulation
 elem = [tElem; sElem(:,[2 3 1]); sElem(:,[4 1 3])];
 T = auxstructure(tElem);
-showmesh(node,elem); hold on;
-showmesh(node,tElem,'Facecolor','y');
 isInterfaceEdge = ((vSign(T.edge(:,1)) == 0) & vSign(T.edge(:,2)) == 0);
 interfaceEdge = T.edge(isInterfaceEdge,:);
-interfaceNode = find(vSign == 0);
-findedge(node,interfaceEdge,'all','noindex','draw');
+nearInterfaceNode = find(vSign == 0);
 
 %% Generate interfaceData
 interface.vSign = vSign;
 interface.tElem = tElem;
 interface.sElem = sElem;
 interface.edge = interfaceEdge;
-interface.node = interfaceNode;
+interface.node = nearInterfaceNode;
--- mesh/interfacemesh2demo.m
+++ mesh/interfacemesh2demo.m
@@ -7,13 +7,15 @@ phi = @(p) sum(p.^2, 2) - 0.5.^2;
 [node,elem,interface] = interfacemesh(box,phi,h);
 showmesh(node,elem);
 findedge(node,interface.edge,'all','noindex','draw');
+pause
 
 %% flower
 [node,elem,interface] = interfacemesh(box,@phiflower,h);
 showmesh(node,elem);
 findedge(node,interface.edge,'all','noindex','draw');
+pause
 
 %% heart
-[node,elem,interface] = interfacemeshdoc(box,@phiheart,0.2*h);
+[node,elem,interface] = interfacemesh(box,@phiheart,0.5*h);
 showmesh(node,elem);
 findedge(node,interface.edge,'all','noindex','draw');
