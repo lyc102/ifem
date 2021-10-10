ff882822fa32754b4175e12bb82aaaecb4ffbebf
S.Cao <scao.math@gmail.com>
2021-10-10 10:21:57 -0500

    Update curl function for Nd0 vector field

--- fem/curlu3.m
+++ fem/curlu3.m
@@ -1,12 +1,18 @@
-function [curlu,volume,curlPhi] = curlu3(node,elem,u)
+function [curlu,volume,curlPhi] = curlu3(node,elem,varargin)
 %% CURLU3 curl of a lowest order Nedelec or linear Nedelec function in 3-D.
 %
 % curlu = curlu3(node,elem,u) compute the gradient of an ND0 or a linear ND
 % vector field  u on a 3-D mesh representing by edge basis 
 % lambda_i Dlambda_j - lambda_j Dlambda_i, lambda_i Dlambda_j + lambda_j Dlambda_i.
+%
+% curlu(t, i, e): the i-th component (i=1:3) of the e-th edge (e=1:6) in
+% elem(t,:).
 % 
 % [curlu,volume,curlPhi] = curlu3(node,elem,u) also outputs volume and curlPhi
-% which is the curl of ND0 basis (additional part of linear ND is curl free). 
+% which is the curl of ND0 basis (additional part of linear ND is curl free).
+%
+% [curlPhi,volume] = curlu3(node,elem) if u is not given, the function only
+% outputs the volume and curlPhi the curl of ND0 basis.
 % 
 % Local node index in a tetrahedron
 %    _1_
@@ -24,12 +30,10 @@ function [curlu,volume,curlPhi] = curlu3(node,elem,u)
 
 %% Sort elem to ascend ordering
 elem = sort(elem,2);
-elem2edge = dof3edge(elem);
+[elem2edge, edge] = dof3edge(elem);
 NT = size(elem,1);
 [volume,elemSign] = simplexvolume(node,elem);
-NE = max(elem2edge(:));
-if length(u) == 2*NE; u = u(1:NE); end
-
+NE = size(edge,1);
 %% Edge vector
 locEdge = [1 2; 1 3; 1 4; 2 3; 2 4; 3 4];
 elem2ve = zeros(NT,3,6);
@@ -49,4 +53,10 @@ curlPhi = curlPhi./repmat(volume.*elemSign,[1 3 6])/3;
 % corrected by multiplying the elemSign
 
 %% curl u is the composition of u and curlPhi
-curlu = dot(permute(repmat(u(elem2edge),[1,1,3]),[1,3,2]),curlPhi,3);
+if nargin > 2
+    u = varargin{1}; 
+    if length(u) == 2*NE; u = u(1:NE); end
+    curlu = dot(permute(repmat(u(elem2edge),[1,1,3]),[1,3,2]),curlPhi,3);
+else
+    curlu = curlPhi;
+end
