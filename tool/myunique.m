146ec572a05df31b44a68143767600651dc5c45c
David-Hang-Du <66042427+David-Hang-Du@users.noreply.github.com>
2022-12-19 15:52:44 -0500

    Update myunique.m

--- tool/myunique.m
+++ tool/myunique.m
@@ -7,9 +7,9 @@ function [sortA,i2,j] = myunique(A)
 %
 % Copyright (C) Long Chen. See COPYRIGHT.txt for details.
 
-matlabversion = version;
-if str2double(matlabversion(end-5:end-2)) <= 2012
-    [sortA, i2, j] = unique(A,'rows');
+matlabversion = extractBetween(version,"(",")");
+if str2double(matlabversion{1}(2:end-1)) > 2012
+    [edge, i2, j] = unique(allEdge,'rows','legacy'); %#ok<ASGLU>
 else
-    [sortA, i2, j] = unique(A,'rows','legacy'); %#ok<*ASGLU>
+    [edge, i2, j] = unique(allEdge,'rows'); %#ok<ASGLU>
 end
