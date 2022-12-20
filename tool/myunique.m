f2b475ce370a2ec624e5f47836722aaa8dbb4a59
David-Hang-Du <66042427+David-Hang-Du@users.noreply.github.com>
2022-12-20 11:06:20 -0500

    Update the extraction of MATLAB version in myunique.m(#9)
    * Update myunique.m
    
    Some MATLAB version has "Update #" at the end, which results in wrong extraction of version number. This change uses parentheses to locate the year.

--- tool/myunique.m
+++ tool/myunique.m
@@ -8,8 +8,11 @@ function [sortA,i2,j] = myunique(A)
 % Copyright (C) Long Chen. See COPYRIGHT.txt for details.
 
 matlabversion = version;
-if str2double(matlabversion(end-5:end-2)) <= 2012
+startIndex = regexp(version,'(')+2;
+endIndex = regexp(version, ')')-2;
+
+if str2double(matlabversion(startIndex:endIndex)) <= 2012
     [sortA, i2, j] = unique(A,'rows');
 else
-    [sortA, i2, j] = unique(A,'rows','legacy'); %#ok<*ASGLU>
+    [sortA, i2, j] = unique(A,'rows','legacy');
 end
